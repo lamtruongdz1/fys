56df3dcea5e4d6885a195c99d301dd498d1deaaa
lamtruongdz <lamtruongdz2000@gmail.com>
2022-03-28 12:50:46 +0700

    add seeder permission

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class YardFactory extends Factory
+class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -14,8 +14,7 @@ class YardFactory extends Factory
     public function definition()
     {
         return [
-            'name',
-            'email'
+            //
         ];
     }
 }
---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+use Database\Factories\UserFactory;
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'Quản lý sân']);
+        Permission::create(['name' => 'Quản lý bài viết']);
+        Permission::create(['name' => 'Quản lý thành viên']);
+        Permission::create(['name' => 'Quản lý nhân sự']);
+
+        // create roles and assign existing permissions
+        $role1 = Role::create(['name' => 'Editor']);
+        $role1->givePermissionTo('Quản lý bài viết');
+        $role1->givePermissionTo('Quản lý sân');
+
+        $role2 = Role::create(['name' => 'admin']);
+        $role2->givePermissionTo('Quản lý sân');
+        $role2->givePermissionTo('Quản lý bài viết');
+        $role2->givePermissionTo('Quản lý thành viên');
+        $role2->givePermissionTo('Quản lý nhân sự');
+
+
+        $role3 = Role::create(['name' => 'Super-Admin']);
+        // gets all permissions via Gate::before rule; see AuthServiceProvider
+
+        // $role4 = Role::create(['name' => 'Staff']);
+        // $role4->givePermissionTo('edit articles');
+        // $role4->givePermissionTo('delete articles');
+
+        // create demo users
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Editor',
+            'email' => 'editor@test.dev',
+        ]);
+        $user->assignRole($role1);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@test.dev',
+        ]);
+        $user->assignRole($role2);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Super-Admin',
+            'email' => 'superadmin@test.dev',
+        ]);
+        $user->assignRole($role3);
+    }
+}
